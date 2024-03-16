b5cadcd0b6023c47efe245709b974252a198cb3a
Camko25 <you@example.com>
2024-03-16 15:51:54 +0100

    update

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
